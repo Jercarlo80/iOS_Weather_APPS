2d47470e085efd0510a1dd7679f806b612e2c40d
jercarlo30 <jloprodaction80@gmail.com>
2022-04-05 22:01:52 +0700

    First Commit

---
+++ Weather_APPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather_APPS
+//
+//  Created by JEREMIA CARLO on 05/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather_APPS/Weather_APPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_APPSApp.swift
+//  Weather_APPS
+//
+//  Created by JEREMIA CARLO on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_APPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
